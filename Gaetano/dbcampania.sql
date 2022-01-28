bcb837c20940ceb59f156328d4d10a146e48e965
jAvengers2021 <gaetanolangella@zerodata.it>
2022-01-28 18:45:22 +0100

    db aggiornato

---
+++ Gaetano/dbcampania.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Gen 28, 2022 alle 18:44
+-- Versione del server: 10.4.22-MariaDB
+-- Versione PHP: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcampania`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `commenti`
+--
+
+CREATE TABLE `commenti` (
+  `id` int(100) NOT NULL,
+  `commento` text NOT NULL,
+  `id_utente` int(100) NOT NULL,
+  `id_documento` int(11) NOT NULL,
+  `data` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `commenti`
+--
+
+INSERT INTO `commenti` (`id`, `commento`, `id_utente`, `id_documento`, `data`) VALUES
+(1, 'WOW che meraviglia!', 1, 4, '2022-01-28'),
+(2, 'Troppo buona!', 2, 4, '2022-01-28');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `documenti`
+--
+
+CREATE TABLE `documenti` (
+  `id` int(100) NOT NULL,
+  `titolo` varchar(100) NOT NULL,
+  `data` date NOT NULL DEFAULT current_timestamp(),
+  `amministratore` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `documenti`
+--
+
+INSERT INTO `documenti` (`id`, `titolo`, `data`, `amministratore`) VALUES
+(4, 'Cibo', '2022-01-27', 0),
+(5, 'Arte', '2022-01-23', 0),
+(6, 'Cultura', '2022-01-11', 0),
+(7, 'Luoghi', '2022-01-28', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `sezioni`
+--
+
+CREATE TABLE `sezioni` (
+  `id` int(11) NOT NULL,
+  `iframe` longtext NOT NULL,
+  `testo` varchar(200) NOT NULL,
+  `id_documento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `sezioni`
+--
+
+INSERT INTO `sezioni` (`id`, `iframe`, `testo`, `id_documento`) VALUES
+(1, '<iframe title=\"Sfogliatella\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=\"https://sketchfab.com/models/4c9aaaf516a84a27962f5ede98315cc6/embed\"> </iframe> </div>', 'Viva la sfogliatella', 4),
+(2, '<iframe title=\"Zeppola con Crema\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=\"https://sketchfab.com/models/54bdfb781bbc406a8fa64f1d777fe22c/embed\"> </iframe> </div>', 'Minkia la zeppola', 4),
+(3, '<iframe title=\"Pompeii,circus,roman,ruins,scan,map\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share  src=\"https://sketchfab.com/models/3c6a1b00abe549909a409e74a416eeef/embed\"> </iframe> </div>', 'Long Time Ago...', 6),
+(4, '<iframe title=\"Tempel of Hera I & II | Italy, Paestum\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=\"https://sketchfab.com/models/6dd6bcf909cd4025b4c073bce7e33ccc/embed\"> </iframe> </div>', 'Paestum e i suoi templi', 6),
+(5, '<iframe title=\"San Francesco di Paola,basilica,napoli,church\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=\"https://sketchfab.com/models/62af40b0f885432d8d713490e617c84d/embed\"> </iframe></div> ', 'Piazza Plebiscito', 7);
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `commenti`
+--
+ALTER TABLE `commenti`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `refdocumento` (`id_documento`);
+
+--
+-- Indici per le tabelle `documenti`
+--
+ALTER TABLE `documenti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `sezioni`
+--
+ALTER TABLE `sezioni`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `refdocumento2` (`id_documento`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `commenti`
+--
+ALTER TABLE `commenti`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT per la tabella `documenti`
+--
+ALTER TABLE `documenti`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT per la tabella `sezioni`
+--
+ALTER TABLE `sezioni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `commenti`
+--
+ALTER TABLE `commenti`
+  ADD CONSTRAINT `refdocumento` FOREIGN KEY (`id_documento`) REFERENCES `documenti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `sezioni`
+--
+ALTER TABLE `sezioni`
+  ADD CONSTRAINT `refdocumento2` FOREIGN KEY (`id_documento`) REFERENCES `documenti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
